ef2ad179ab388b088764417dcf7100ff966bd6aa
frankymacster <fs.sunatori@me.com>
2015-08-31 14:07:39 -0400

    Create new classes for map itinerary feature.

---
+++ GitUseExample/NSObject+MapForItenerary.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+MapForItenerary.h
+//  GitUseExample
+//
+//  Created by François Sunatori on 2015-08-31.
+//  Copyright (c) 2015 François Sunatori. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (MapForItenerary)
+
+@end
---
+++ GitUseExample/NSObject+MapForItenerary.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+MapForItenerary.m
+//  GitUseExample
+//
+//  Created by François Sunatori on 2015-08-31.
+//  Copyright (c) 2015 François Sunatori. All rights reserved.
+//
+
+#import "NSObject+MapForItenerary.h"
+
+@implementation NSObject (MapForItenerary)
+
+@end
