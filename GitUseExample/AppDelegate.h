8aec58663a7973cbf8e88e89bbe47285060e2527
frankymacster <fs.sunatori@me.com>
2015-08-31 13:46:18 -0400

    Initial Commit

---
+++ GitUseExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitUseExample
+//
+//  Created by François Sunatori on 2015-08-31.
+//  Copyright (c) 2015 François Sunatori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitUseExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitUseExample
+//
+//  Created by François Sunatori on 2015-08-31.
+//  Copyright (c) 2015 François Sunatori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
